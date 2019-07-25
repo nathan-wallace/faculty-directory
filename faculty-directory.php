a759c89d7ca619e7bee2d9ff8507a2bb89e41b59
Nathan Wallace <n.wallace@northeastern.edu>
2019-07-24 20:27:49 -0400

    github updater plugin URI

--- faculty-directory.php
+++ faculty-directory.php
@@ -14,7 +14,8 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Faculty and Staff Directory
- * Plugin URI:        http://example.com/faculty-directory-uri/
+ * Plugin URI:        http://wallacecreative.agency/
+ * GitHub Plugin URI: https://github.com/afragen/github-updater
  * Description:       Adds a Profile content-type and associated Gutenberg blocks for disaplying faculty and staff.
  * Version:           1.0.0
  * Author:            Wallace Creative
